cbd720e9767fcf79d7edc84f88730057a810fc9b
Luis Hevia <luixvix@gmail.com>
2023-09-20 21:34:11 +0200

    Add basic slides config, theme and Globals script

---
+++ godot_presentation/Globals.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var currentSlide = 1
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_right"):
+		currentSlide += 1
+		var error = get_tree().change_scene("res://slides/" + ("%02d" % currentSlide) + ".tscn")
+		if error != OK:
+			currentSlide -= 1
+			print("Reached the end of the presentation")
+	if Input.is_action_just_pressed("ui_left"):
+		currentSlide -= 1
+		var error = get_tree().change_scene("res://slides/" + ("%02d" % currentSlide) + ".tscn")
+		if error != OK:
+			currentSlide += 1
+			print("Reached the start of the presentation")
+	if Input.is_action_just_pressed("ui_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
